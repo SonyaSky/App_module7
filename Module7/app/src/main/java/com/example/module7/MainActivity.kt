dcc13b5acb5749088c1d9bb6d6bc774568f946fa
PolinaAdashkevich <polinatulenka@gmail.com>
2024-05-06 19:25:47 +0300

    ура оно работает ура урааа
    приложение запускается, дает выбрать картинку из галереи и отображает ее!

--- Module7/app/src/main/java/com/example/module7/MainActivity.kt
+++ Module7/app/src/main/java/com/example/module7/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pickimage
+package com.example.module7
 
 import android.app.Activity
 import android.content.Intent
@@ -13,7 +13,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity() {
     private lateinit var pickImage: FloatingActionButton
     private lateinit var selectedImage: AppCompatImageView
-
+    private val changeImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            val imgUri = data?.data
+            binding.selectedImage.setImageURI(imgUri)
+        }
+    }
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +30,6 @@ class MainActivity : AppCompatActivity() {
         // Use the binding object to reference the views in your layout
         binding.pickImage.setOnClickListener {
             val pickImg = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-            val changeImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                if (it.resultCode == Activity.RESULT_OK) {
-                    val data = it.data
-                    val imgUri = data?.data
-                    binding.selectedImage.setImageURI(imgUri)
-                }
-            }
             changeImage.launch(pickImg)
         }
     }
